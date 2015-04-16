0e24245204f0c3c88ed10c6c5f8a8f7be29e114d
Quentin Madec <quentin.madec@datadoghq.com>
2015-04-16 15:57:34 -0400

    [core] use internal `/run` for temp files
    Added it to pidfile (instead of `/var/run/dd-agent`) and pickle
    files, as it should fix all issues with `dd-agent info` reporting
    incorrectly. It doesn't break anything, as it checks first to see if
    the internal `/run` exists, and fallback to the current behaviour if
    not. (ie `tempfile.gettempdir()`)
    
    Also:
    - move PidFile and Platform from util.py to their own files in `utils/`
    - as a consequence, move `checks/utils.py` into `utils/tailfile.py`
    
    f pidfile

--- setup.py
+++ setup.py
@@ -150,7 +150,6 @@ elif sys.platform == 'darwin':
             'dogstream',
             'images',
             'status.html',
-            'utils',
         ],
         'options': {
             'py2app': {
